e482b1eeaaea1b83bf879921fbced8060697ebd6
SaiMV <saimv09@gmail.com>
2021-10-27 00:34:01 +0530

    3 files modified

--- src/app/AngGui/nav-bar/circular/nav-bar-circular.component.ts
+++ src/app/AngGui/nav-bar/circular/nav-bar-circular.component.ts
@@ -5,4 +5,30 @@ import { Component } from '@angular/core';
   templateUrl: './nav-bar-circular.component.html',
   styleUrls: ['./nav-bar-circular.component.css'],
 })
-export class NavBarCircularComponent {}
+export class NavBarCircularComponent {
+  srart_pos = 90.75;
+  item_count = 13;
+  s = (0.52 * Math.PI) / 180;
+  pos = [];
+  elem = document.getElementsByClassName('item');
+  constructor() {
+    this.allocationItems();
+  }
+  allocationItems() {
+    let i;
+    let pp = this.elem[6]
+      ?.getElementsByTagName('a')[0]
+      .getAttribute('data-img');
+    document.getElementById('pic').style.backgroundImage = "url('" + pp + "')";
+    document.getElementById('pic').className = 'img-box';
+    this.pos[0] = this.srart_pos;
+    for (i = 1; i < this.item_count; i++) {
+      this.pos[i] = this.pos[i - 1] - 0.2;
+      this.srart_pos = this.pos[i];
+    }
+    for (i = 0; i < this.item_count + 1; i++) {
+      // this.elem[i].style.left = 240 + 250 * Math.sin(this.pos[i]) + 'px';
+      // this.elem[i].style.top = 240 + 250 * Math.cos(this.pos[i]) + 'px';
+    }
+  }
+}
