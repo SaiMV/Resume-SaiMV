eb8196b24fee896f304d78e26a97c755d2ce3841
SaiMV <saimv09@gmail.com>
2021-10-27 00:12:49 +0530

    2 files modified, 3 added

---
+++ src/app/AngGui/nav-bar/circular/nav-bar-circular.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'circular-nav',
+  templateUrl: './nav-bar-circular.component.html',
+  styleUrls: ['./nav-bar-circular.component.css'],
+})
+export class NavBarCircularComponent {}
