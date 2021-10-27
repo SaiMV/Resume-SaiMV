3ac27d7ff8e9a6217be413b00e2183b76f26eadf
SaiMV <saimv09@gmail.com>
2021-10-27 20:06:38 +0530

    5 files modified, 1 deleted

--- src/app/AngGui/nav-bar/circular/nav-bar-circular.component.ts
+++ src/app/AngGui/nav-bar/circular/nav-bar-circular.component.ts
@@ -22,74 +22,75 @@ export class NavBarCircularComponent implements OnInit {
   }
 
   allocationItems() {
-    let i;
-    let pp = this.elem[6]
+    var i;
+    var pp = this.elem[6]
       ?.getElementsByTagName('a')[0]
       .getAttribute('data-img');
     document.getElementById('pic').style.backgroundImage = "url('" + pp + "')";
     document.getElementById('pic').className = 'img-box';
     this.pos[0] = this.srart_pos;
-    console.log('11111', this.item_count);
     for (i = 1; i < this.item_count; i++) {
       this.pos[i] = this.pos[i - 1] - 0.2;
-      this.srart_pos = this.pos[i];
     }
     for (i = 0; i < this.item_count + 1; i++) {
-
       let a = this.elem[i] as HTMLElement;
-            a.style.left = 240 + 250 * Math.sin(this.pos[i]) + 'px';
+      a.style.left = 240 + 250 * Math.sin(this.pos[i]) + 'px';
       a.style.top = 240 + 250 * Math.cos(this.pos[i]) + 'px';
     }
   }
   animation(args, flag) {
-    var $ = {
-      radius: 250,
-      speed: 10,
-    };
-    var e = this.elem;
-    document.getElementById('pic').className = 'hide';
-    function animate(draw, duration, callback) {
-      var start = performance.now();
-      requestAnimationFrame(function animate(time) {
-        var timePassed = time - start;
-        console.log(time, start);
-        if (timePassed > duration) timePassed = duration;
-        draw(timePassed);
-        if (timePassed < duration) {
-          requestAnimationFrame(animate);
-        } else callback();
-      });
-    }
-    animate(
-      (timePassed) => {
-        var i;
+    try {
+      var $ = {
+        radius: 250,
+        speed: 10,
+      };
+      var e = this.elem;
+      document.getElementById('pic').className = 'hide';
+      function animate(draw, duration, callback) {
+        var start = performance.now();
+        requestAnimationFrame(function animate(time) {
+          var timePassed = time - start;
+          console.log(time, start);
+          if (timePassed > duration) timePassed = duration;
+          draw(timePassed);
+          if (timePassed < duration) {
+            requestAnimationFrame(animate);
+          } else callback();
+        });
+      }
+      animate(
+        (timePassed) => {
+          var i;
 
-        console.log(this.item_count);
+          console.log(this.item_count);
 
-        for (i = 0; i < this.item_count; i++) {
-          console.log()
-          let a = this.elem[i] as HTMLElement;
+          for (i = 0; i < this.item_count; i++) {
+            console.log();
+            let a = this.elem[i] as HTMLElement;
             a.style.left = 240 + $.radius * Math.sin(this.pos[i]) + 'px';
             a.style.top = 240 + $.radius * Math.cos(this.pos[i]) + 'px';
+            if (flag) {
+              this.pos[i] += this.s;
+            } else {
+              this.pos[i] -= this.s;
+            }
+          } /* callback function */
+        },
+        400,
+        () => {
+          var list = document.getElementById('list');
+          var ch = flag ? list.firstElementChild : list.lastElementChild;
+          ch.remove();
           if (flag) {
-            this.pos[i] += this.s;
+            list.appendChild(ch);
           } else {
-            this.pos[i] -= this.s;
+            list.insertBefore(ch, list.firstChild);
           }
-        } /* callback function */
-      },
-      400,
-      () => {
-        var list = document.getElementById('list');
-        var ch = flag ? list.firstElementChild : list.lastElementChild;
-        ch.remove();
-        if (flag) {
-          list.appendChild(ch);
-        } else {
-          list.insertBefore(ch, list.firstChild);
+          this.allocationItems();
         }
-        this.allocationItems();
-      }
-    );
+      );
+    } catch (errr) {
+      console.log('error1 ', errr);
+    }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { NavBarCircularComponent } from './AngGui/nav-bar/circular/nav-bar-circular.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, NavBarCircularComponent],
+  declarations: [AppComponent, NavBarCircularComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
