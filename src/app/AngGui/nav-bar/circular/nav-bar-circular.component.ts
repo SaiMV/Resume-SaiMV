99afe4b333f2db94d500055e31bd88f5eecfa70a
SaiMV <saimv09@gmail.com>
2021-10-27 23:01:39 +0530

    5 files modified, 3 added

--- src/app/AngGui/nav-bar/circular/nav-bar-circular.component.ts
+++ src/app/AngGui/nav-bar/circular/nav-bar-circular.component.ts
@@ -12,12 +12,7 @@ export class NavBarCircularComponent implements OnInit {
   pos = [];
   elem = document.getElementsByClassName('item');
 
-  constructor() {
-    //this.allocationItems();
-  }
-
   ngOnInit() {
-    // console.log(this.elem);
     this.allocationItems();
   }
 
@@ -39,58 +34,53 @@ export class NavBarCircularComponent implements OnInit {
     }
   }
   animation(args, flag) {
-    try {
-      var $ = {
-        radius: 250,
-        speed: 10,
-      };
-      var e = this.elem;
-      document.getElementById('pic').className = 'hide';
-      function animate(draw, duration, callback) {
-        var start = performance.now();
-        requestAnimationFrame(function animate(time) {
-          var timePassed = time - start;
-          console.log(time, start);
-          if (timePassed > duration) timePassed = duration;
-          draw(timePassed);
-          if (timePassed < duration) {
-            requestAnimationFrame(animate);
-          } else callback();
-        });
-      }
-      animate(
-        (timePassed) => {
-          var i;
-
-          console.log(this.item_count);
-
-          for (i = 0; i < this.item_count; i++) {
-            console.log();
-            let a = this.elem[i] as HTMLElement;
-            a.style.left = 240 + $.radius * Math.sin(this.pos[i]) + 'px';
-            a.style.top = 240 + $.radius * Math.cos(this.pos[i]) + 'px';
-            if (flag) {
-              this.pos[i] += this.s;
-            } else {
-              this.pos[i] -= this.s;
-            }
-          } /* callback function */
-        },
-        400,
-        () => {
-          var list = document.getElementById('list');
-          var ch = flag ? list.firstElementChild : list.lastElementChild;
-          ch.remove();
-          if (flag) {
-            list.appendChild(ch);
-          } else {
-            list.insertBefore(ch, list.firstChild);
-          }
-          this.allocationItems();
-        }
-      );
-    } catch (errr) {
-      console.log('error1 ', errr);
+    var $ = {
+      radius: 250,
+      speed: 10,
+    };
+    var e = this.elem;
+    document.getElementById('pic').className = 'hide';
+    function animate(draw, duration, callback) {
+      var start = performance.now();
+      requestAnimationFrame(function animate(time) {
+        var timePassed = time - start;
+        if (timePassed > duration) timePassed = duration;
+        draw(timePassed);
+        if (timePassed < duration) {
+          requestAnimationFrame(animate);
+        } else callback();
+      });
     }
+    animate(
+      (timePassed) => {
+        var i;
+
+        console.log(this.item_count);
+
+        for (i = 0; i < this.item_count; i++) {
+          console.log();
+          let a = this.elem[i] as HTMLElement;
+          a.style.left = 240 + $.radius * Math.sin(this.pos[i]) + 'px';
+          a.style.top = 240 + $.radius * Math.cos(this.pos[i]) + 'px';
+          if (flag) {
+            this.pos[i] += this.s;
+          } else {
+            this.pos[i] -= this.s;
+          }
+        }
+      },
+      400,
+      () => {
+        var list = document.getElementById('list');
+        var ch = flag ? list.firstElementChild : list.lastElementChild;
+        ch.remove();
+        if (flag) {
+          list.appendChild(ch);
+        } else {
+          list.insertBefore(ch, list.firstChild);
+        }
+        this.allocationItems();
+      }
+    );
   }
 }
