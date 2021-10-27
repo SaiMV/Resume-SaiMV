588dacb5554196427289d037d6e893d224234574
SaiMV <saimv09@gmail.com>
2021-10-27 13:30:26 +0530

    2 files modified

--- src/app/AngGui/nav-bar/circular/nav-bar-circular.component.ts
+++ src/app/AngGui/nav-bar/circular/nav-bar-circular.component.ts
@@ -1,19 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'circular-nav',
   templateUrl: './nav-bar-circular.component.html',
   styleUrls: ['./nav-bar-circular.component.css'],
 })
-export class NavBarCircularComponent {
+export class NavBarCircularComponent implements OnInit {
   srart_pos = 90.75;
   item_count = 13;
   s = (0.52 * Math.PI) / 180;
   pos = [];
   elem = document.getElementsByClassName('item');
+
   constructor() {
+    //this.allocationItems();
+  }
+
+  ngOnInit() {
+    // console.log(this.elem);
     this.allocationItems();
   }
+
   allocationItems() {
     let i;
     let pp = this.elem[6]
@@ -22,13 +29,61 @@ export class NavBarCircularComponent {
     document.getElementById('pic').style.backgroundImage = "url('" + pp + "')";
     document.getElementById('pic').className = 'img-box';
     this.pos[0] = this.srart_pos;
+    console.log(this.item_count);
     for (i = 1; i < this.item_count; i++) {
       this.pos[i] = this.pos[i - 1] - 0.2;
       this.srart_pos = this.pos[i];
     }
     for (i = 0; i < this.item_count + 1; i++) {
-      // this.elem[i].style.left = 240 + 250 * Math.sin(this.pos[i]) + 'px';
-      // this.elem[i].style.top = 240 + 250 * Math.cos(this.pos[i]) + 'px';
+      //this.elem[i].style.left = 240 + 250 * Math.sin(this.pos[i]) + 'px';
+      //this.elem[i].style.top = 240 + 250 * Math.cos(this.pos[i]) + 'px';
     }
+  }
+  animation(args, flag) {
+    var $ = {
+      radius: 250,
+      speed: 10,
+    };
+    var e = this.elem;
+    document.getElementById('pic').className = 'hide';
+    function animate(draw, duration, callback) {
+      var start = performance.now();
+      requestAnimationFrame(function animate(time) {
+        var timePassed = time - start;
+        console.log(time, start);
+        if (timePassed > duration) timePassed = duration;
+        draw(timePassed);
+        if (timePassed < duration) {
+          requestAnimationFrame(animate);
+        } else callback();
+      });
+    }
+    animate(
+      function (timePassed) {
+        var i;
+        console.log(this.item_count);
+        for (i = 0; i < this.item_count; i++) {
+          // e[i].style.left = 240 + $.radius * Math.sin(this.pos[i]) + 'px';
+          // e[i].style.top = 240 + $.radius * Math.cos(this.pos[i]) + 'px';
+          if (flag) {
+            this.pos[i] += this.s;
+          } else {
+            this.pos[i] -= this.s;
+          }
+        } /* callback function */
+      },
+      400,
+      function changeItems() {
+        var list = document.getElementById('list');
+        var ch = flag ? list.firstElementChild : list.lastElementChild;
+        ch.remove();
+        if (flag) {
+          list.appendChild(ch);
+        } else {
+          list.insertBefore(ch, list.firstChild);
+        }
+        this.allocationItems();
+      }
+    );
   }
 }
