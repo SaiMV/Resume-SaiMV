863d20109df30125cab3ee8acca0b784cce4859a
SaiMV <saimv09@gmail.com>
2021-10-29 00:15:11 +0530

    3 files modified

--- src/app/AngGui/progress-bar/progress-bar.component.ts
+++ src/app/AngGui/progress-bar/progress-bar.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'progress-bar',
   templateUrl: './progress-bar.component.html',
   styleUrls: ['./progress-bar.component.css'],
 })
-export class ProgressBarComponent {}
+export class ProgressBarComponent {
+  @Input() type: string = 'nomal';
+  @Input() title: string = '';
+  @Input() progressPercentage: number = 100;
+  constructor() {}
+  getCircularPercentage() {
+    return this.progressPercentage > 0 && this.progressPercentage <= 50
+      ? this.progressPercentage * 3.6
+      : (this.progressPercentage - 50) * 3.6;
+  }
+}
