8929ab0bbb6a9ab048dd80d18e6dfc3896911cba
SaiMV <saimv09@gmail.com>
2021-11-02 00:41:49 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,23 +9,25 @@ export class AppComponent {
   @ViewChild('mainContainer') mainContainer: any;
   flashScreen = true;
   constructor() {
-    // setTimeout(() => {
-    //   this.flashScreen = false;
-    //   this.intervalTranslation1 = setInterval(() => {
-    //     this.setElementOpacity();
-    //   }, 2);
-    // }, 2300);
+    setTimeout(() => {
+      this.flashScreen = false;
+      this.intervalTranslation1 = setInterval(() => {
+        this.setElementOpacity();
+      }, 2);
+    }, 2300);
     //     window.open("http://localhost:4200/", "full", "dependent=yes, fullscreen=yes")
   }
   intervalTranslation1: any;
   opacityupto: any = 0;
   setElementOpacity() {
-    const elm = this.mainContainer.nativeElement;
-    this.opacityupto += 0.01;
-    if (this.opacityupto > 1) {
-      clearInterval(this.intervalTranslation1);
-      return;
+    const elm = this.mainContainer?.nativeElement;
+    if (elm) {
+      this.opacityupto += 0.01;
+      if (this.opacityupto > 1) {
+        clearInterval(this.intervalTranslation1);
+        return;
+      }
+      elm.style.opacity = this.opacityupto;
     }
-    elm.style.opacity = this.opacityupto;
   }
 }
