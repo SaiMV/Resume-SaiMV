5b583f9cbd90d2df695c04c22ad2d77c1e1f8413
SaiMV <saimv09@gmail.com>
2021-11-02 00:56:17 +0530

    2 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutMeComponent } from './pages/about-me/about-me.component';
+import { EducationDetailsComponent } from './pages/education-details/education-details.component';
 import { HomeScreenComponent } from './pages/home-screen/home-screen.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/', pathMatch: 'full' },
-  { path: '', component: HomeScreenComponent },
+  { path: '', component: EducationDetailsComponent },
   { path: 'about-me', component: AboutMeComponent },
+  { path: 'education-details', component: EducationDetailsComponent },
 ];
 
 @NgModule({
