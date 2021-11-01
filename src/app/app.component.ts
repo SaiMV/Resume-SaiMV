eaef692097917901b0752552227d31404035cf3b
SaiMV <saimv09@gmail.com>
2021-11-02 00:26:42 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  @ViewChild('mainContainer') mainContainer: any;
+  flashScreen = true;
+  constructor() {
+    setTimeout(() => {
+      this.flashScreen = false;
+      this.intervalTranslation1 = setInterval(() => {
+        this.setElementOpacity();
+      }, 2);
+    }, 2300);
+
+    //     window.open("http://localhost:4200/", "full", "dependent=yes, fullscreen=yes")
+  }
+  intervalTranslation1: any;
+  opacityupto: any = 0;
+  setElementOpacity() {
+    const elm = this.mainContainer.nativeElement;
+    this.opacityupto += 0.01;
+    if (this.opacityupto > 1) {
+      clearInterval(this.intervalTranslation1);
+      return;
+    }
+    elm.style.opacity = this.opacityupto;
+  }
 }
