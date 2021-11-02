357d8714e12646c44299da57abcb8c71b1f3877b
SaiMV <saimv09@gmail.com>
2021-11-02 15:38:32 +0530

    3 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutMeComponent } from './pages/about-me/about-me.component';
+import { CompanyExperienceDetailsComponent } from './pages/company-experience-details/company-experience-details.component';
 import { EducationDetailsComponent } from './pages/education-details/education-details.component';
 import { HomeScreenComponent } from './pages/home-screen/home-screen.component';
 import { PersonalDetailsComponent } from './pages/personal-details/personal-details.component';
@@ -8,12 +9,16 @@ import { ProjectDetailsComponent } from './pages/project-details/project-details
 
 const routes: Routes = [
   { path: '', redirectTo: '/', pathMatch: 'full' },
-  { path: '', component: ProjectDetailsComponent },
+  { path: '', component: CompanyExperienceDetailsComponent },
   { path: 'home', component: HomeScreenComponent },
   { path: 'aboutMe', component: AboutMeComponent },
   { path: 'educationDetails', component: EducationDetailsComponent },
   { path: 'personalDetails', component: PersonalDetailsComponent },
   { path: 'projectDetails', component: ProjectDetailsComponent },
+  {
+    path: 'companyExperienceDetailsComponent',
+    component: CompanyExperienceDetailsComponent,
+  },
 ];
 
 @NgModule({
